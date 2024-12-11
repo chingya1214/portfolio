c739166a134faa1bb29ef384ac70f426fd86c368
chingya1214 <adt105115@gm.ntcu.edu.tw>
2024-12-11 17:11:24 +0800

    update page
    update page

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
+  base: "./",
   plugins: [vue()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler", // or 'modern'
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"), // 主頁
+        about: resolve(__dirname, "about.html"), // 關於頁
+      },
+    },
+  },
 });
